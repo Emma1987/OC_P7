fa64084f273b2731978f6bae48a27c33d8bec275
Emma1987 <mercadal.em@gmail.com>
2018-05-10 17:45:51 +0200

    Register nelmio api doc bundle and its config

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     Bazinga\Bundle\HateoasBundle\BazingaHateoasBundle::class => ['all' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
 ];
